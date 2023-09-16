e40eaf4feb76bb5995ff62ff5cf55d845aec1cd7
machineke <markarapsoi72@gmail.com>
2023-09-16 23:54:33 +0300

    created a javascript program to convert human years to dog years

---
+++ Javascript/dogYears.js
@@ -0,0 +1,22 @@
+//creates a variable to store my age
+let myAge = 21;
+
+//creates a variable and sets its value
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+//creates a variable and computes its value
+let laterYears = myAge -2;
+
+//computes the later years
+laterYears *= 4;
+
+//computes my age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+//creates a variable to store my name and converts it lowercase
+let myName = "Mark";
+myname = myName.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`);
