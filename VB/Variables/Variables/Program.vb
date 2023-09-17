507c75920609df5d0be56e4c6bfd9291d5b4f410
machineke <markarapsoi72@gmail.com>
2023-09-18 00:29:18 +0300

    Visual Basic variables

---
+++ VB/Variables/Variables/Program.vb
@@ -0,0 +1,33 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        'Dim x As Integer
+        'Dim y As Integer
+
+        'x = 7
+        'y = x + 3
+        'Console.WriteLine("Y Is " & y)
+
+        'Dim myFirstName As String
+        'myFirstName = "Mark"
+        'Dim myFirstName As String = "Mark"
+
+        'Dim myFirstName = "Mark"
+        'Console.WriteLine("My name is " & myFirstName)
+
+        Dim x As Integer = 7
+        Dim y As String = "Mark"
+        Dim y As Integer = 5
+        'Dim myFirstTry As String = CStr(x) & y.ToUpper()
+        'Console.WriteLine(myFirstTry)
+
+        'Dim mySecondTry As Integer = x + CInt(y)
+        'Console.WriteLine(mySecondTry)
+
+
+        Console.ReadLine()
+
+    End Sub
+End Module
